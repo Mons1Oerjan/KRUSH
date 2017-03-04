9e310372a9e58d69d2838c2e53c337d51c19ebaa
Eric Desjardins <eric.desjardins11@gmail.com>
2017-03-04 12:33:05 -0400

    Added logo and slogan textview

--- app/src/main/java/cs/dal/krush/MainActivity.java
+++ app/src/main/java/cs/dal/krush/MainActivity.java
@@ -1,10 +1,12 @@
 package cs.dal.krush;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         //fetch UI elements:
         final Button tutor_home_button = (Button) findViewById(R.id.tutor_login_button);
         final Button student_home_button = (Button) findViewById(R.id.student_login_button);
+        final TextView krush_logo_textView = (TextView) findViewById(R.id.krushLogo);
+        final TextView krush_slogan_textView = (TextView) findViewById(R.id.krushSlogan);
+
+        //fetch custom app font
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/FredokaOne-Regular.ttf");
+
+        //set logo font style
+        krush_logo_textView.setTypeface(typeFace);
+        krush_slogan_textView.setTypeface(typeFace);
 
         //setup OnClickListeners:
         tutor_home_button.setOnClickListener(new View.OnClickListener() {
