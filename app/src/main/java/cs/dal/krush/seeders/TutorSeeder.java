af702cf9c87dac0d0579d6b2140f6f8fed35e6d4
Orjan Monsen <mons@dal.ca>
2017-03-13 17:33:55 -0300

    student-home-tutor-list: added image to tutor seeders

--- app/src/main/java/cs/dal/krush/seeders/TutorSeeder.java
+++ app/src/main/java/cs/dal/krush/seeders/TutorSeeder.java
@@ -14,14 +14,14 @@ public class TutorSeeder {
      * @param db
      */
     public static void insert(DBHelper db){
-        db.tutor.insert(1,1,"sample1","Greg","Miller","gregpmillr@gmail.com","password",2);
-        db.tutor.insert(3,2,"sample2","Michael","Casey","mc@dal.ca","password",2);
-        db.tutor.insert(2,1,"sample3","Orjan","Monsen","om@dal.ca","password",5);
-        db.tutor.insert(4,5,"sample4","Eric","Desjardins","ed@dal.ca","password",1);
-        db.tutor.insert(5,3,"sample5","Connor","Walsh","cw@dal.ca","password",4);
-        db.tutor.insert(2,2,"sample6","Jack","Reaper","jr@dal.ca","password",2);
-        db.tutor.insert(5,4,"sample7","Peter","Richards","pr@gmail.com","password",2);
-        db.tutor.insert(1,3,"sample8","Bonnie","Trimper","br@gmail.com","password",2);
-        db.tutor.insert(1,3,"sample9","Eve","Zinck","ez@gmail.com","password",2);
+        db.tutor.insert(1,1,"egg1","Greg","Miller","gregpmillr@gmail.com","password",2);
+        db.tutor.insert(3,2,"egg1","Michael","Casey","mc@dal.ca","password",2);
+        db.tutor.insert(2,1,"egg1","Orjan","Monsen","om@dal.ca","password",5);
+        db.tutor.insert(4,5,"egg1","Eric","Desjardins","ed@dal.ca","password",1);
+        db.tutor.insert(5,3,"egg1","Connor","Walsh","cw@dal.ca","password",4);
+        db.tutor.insert(2,2,"egg1","Jack","Reaper","jr@dal.ca","password",2);
+        db.tutor.insert(5,4,"egg1","Peter","Richards","pr@gmail.com","password",2);
+        db.tutor.insert(1,3,"egg1","Bonnie","Trimper","br@gmail.com","password",2);
+        db.tutor.insert(1,3,"egg1","Eve","Zinck","ez@gmail.com","password",2);
     }
 }
