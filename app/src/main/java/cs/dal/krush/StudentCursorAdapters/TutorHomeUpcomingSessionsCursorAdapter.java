c14a9c935a6945479e6bcbebb3b41614abe1d9df
Michael Casey <m.casey@dal.ca>
2017-03-16 14:37:29 -0300

    Adjusted layouts to be dynamic depending on phone size. Modified text appearance on headers

---
+++ app/src/main/java/cs/dal/krush/StudentCursorAdapters/TutorHomeUpcomingSessionsCursorAdapter.java
@@ -0,0 +1,85 @@
+package cs.dal.krush.StudentCursorAdapters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import cs.dal.krush.R;
+
+/**
+ * This is the adapter class for the customized rows in the Upcoming Sessions
+ * list view on the tutor home page.
+ *
+ * Source:
+ * [4] C. (n.d.). Codepath/android_guides. Retrieved March 12, 2017,
+ * from https://github.com/codepath/android_guides/wiki/Populating-a-ListView-with-a-CursorAdapter
+ */
+
+public class TutorHomeUpcomingSessionsCursorAdapter extends CursorAdapter {
+
+    //store the context set in the constructor
+    private Context mContext;
+
+    /**
+     * Constructor that initializes the custom cursor adapter.
+     *
+     * @param context
+     * @param cursor
+     */
+    public TutorHomeUpcomingSessionsCursorAdapter(Context context, Cursor cursor){
+        super(context, cursor, 0);
+        this.mContext = context;
+    }
+
+    /**
+     * Overrides the newView method used to inflate a new view and return it.
+     *
+     * @param context
+     * @param cursor
+     * @param parent
+     * @return
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent){
+        return LayoutInflater.from(context).inflate(R.layout.student_home_listentry, parent, false);
+    }
+
+    /**
+     * Overrides the bindView method used to bind all data to a given view.
+     *
+     * @param view
+     * @param context
+     * @param cursor
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor){
+        //fetch UI components:
+        TextView header = (TextView)view.findViewById(R.id.firstLine);
+        TextView subHeader = (TextView)view.findViewById(R.id.secondLine);
+        ImageView image = (ImageView)view.findViewById(R.id.icon);
+
+        //Get the student's profile image:
+        String imageFileName = cursor.getString(cursor.getColumnIndexOrThrow("profile_pic"));
+        if (imageFileName != null){
+            int resourceId = mContext.getResources().getIdentifier(
+                    imageFileName, "drawable", mContext.getPackageName());
+            image.setImageResource(resourceId);
+        }
+
+        //Set the row's header text:
+        String title = cursor.getString(cursor.getColumnIndexOrThrow("title"));
+        header.setText(title);
+
+        //Set the row's sub-header text:
+        String studentFirstName = cursor.getString(cursor.getColumnIndexOrThrow("f_name"));
+        String studentLastName = cursor.getString(cursor.getColumnIndexOrThrow("l_name"));
+        String studentSchool = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+        String text2content = studentFirstName + " " + studentLastName + ", University: " + studentSchool;
+        subHeader.setText(text2content);
+    }
+}
--- app/src/main/java/cs/dal/krush/tutorFragments/TutorHomeFragment.java
+++ app/src/main/java/cs/dal/krush/tutorFragments/TutorHomeFragment.java
@@ -1,6 +1,7 @@
 package cs.dal.krush.tutorFragments;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,11 +13,16 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import cs.dal.krush.R;
+import cs.dal.krush.StudentCursorAdapters.TutorHomeUpcomingSessionsCursorAdapter;
 import cs.dal.krush.models.DBHelper;
 
 /**
  * Sets up the Tutor Home fragment. This fragment belongs to the TutorMainActivity class
  * and is accessed through the tutor's bottom navigation bar.
+ *
+ *  Source:
+ * [5] List View. (n.d.). Retrieved March 12, 2017,
+ * from https://developer.android.com/guide/topics/ui/layout/listview.html
  */
 public class TutorHomeFragment extends Fragment {
 
@@ -34,12 +40,19 @@ public class TutorHomeFragment extends Fragment {
         //init DB connection:
         DBHelper mydb = new DBHelper(C);
 
+        Cursor tutor = mydb.tutor.getData(userId);
+        tutor.moveToFirst();
+
         //fetch UI elements:
         ListView upcomingSessionsListView = (ListView)view.findViewById(R.id.upcomingSessionsListView);
         RatingBar tutorRating = (RatingBar) view.findViewById(R.id.rating);
         TextView pageTitle = (TextView)view.findViewById(R.id.homeTitleLabel);
         TextView sessionsLabel = (TextView)view.findViewById(R.id.upcomingSessionsLabel);
         TextView ratingTitle = (TextView) view.findViewById(R.id.tutorRating);
+
+        String currentTutorRating = tutor.getString(tutor.getColumnIndex("rating"));
+        if (currentTutorRating != null)
+            tutorRating.setRating(Float.parseFloat(currentTutorRating));
 
         //fetch custom app font:
         Typeface typeFace = Typeface.createFromAsset(getActivity().getAssets(),"fonts/FredokaOne-Regular.ttf");
@@ -48,6 +61,13 @@ public class TutorHomeFragment extends Fragment {
         pageTitle.setTypeface(typeFace);
         sessionsLabel.setTypeface(typeFace);
         ratingTitle.setTypeface(typeFace);
+
+        //get all tutoring sessions by the tutor:
+        Cursor cursorSessionsResponse = mydb.tutoringSession.getDataByTutorIdForCursorAdapter(userId);
+
+        //set sessions listview adapter:
+        TutorHomeUpcomingSessionsCursorAdapter sessionsAdapter = new TutorHomeUpcomingSessionsCursorAdapter(C, cursorSessionsResponse);
+        upcomingSessionsListView.setAdapter(sessionsAdapter);
 
         return view;
     }
