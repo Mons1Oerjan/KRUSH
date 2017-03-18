f55c52edb19005251b6c8b9ff87eabfc4eb64051
Eric Desjardins <eric.desjardins11@gmail.com>
2017-03-18 10:54:17 -0300

    Added validation

--- app/src/main/java/cs/dal/krush/studentFragments/StudentPaymentFragment.java
+++ app/src/main/java/cs/dal/krush/studentFragments/StudentPaymentFragment.java
@@ -1,21 +1,85 @@
 package cs.dal.krush.studentFragments;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import cs.dal.krush.R;
 
 public class StudentPaymentFragment extends Fragment {
+
+    private boolean isValid;
+    private String creditCarNumber;
+    private String cvvNumbers;
+    private String expirationMonth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.student_profile, container, false);
-        int userId = Integer.parseInt(getArguments().getString("UserID"));
+        View view = inflater.inflate(R.layout.student_payment, container, false);
+        // TODO: 2017-03-17 Get student and tutor ID from bundle
+//        int userId = Integer.parseInt(getArguments().getString("UserID"));
+//        int tutorId = Integer.parseInt(getArguments().getString("TutorID"));
 
+        //fetch UI components
+        final TextView pageHeader = (TextView) view.findViewById(R.id.paymentHeader);
+        final TextView costLabel = (TextView) view.findViewById(R.id.costLabel);
+        final TextView tutoringCost = (TextView) view.findViewById(R.id.tutoringCost);
+        final TextView cardNumberLabel = (TextView) view.findViewById(R.id.cardNumberLabel);
+        final TextView expDateLabel = (TextView) view.findViewById(R.id.expDateLabel);
+        final TextView cvvLabel = (TextView) view.findViewById(R.id.cvvLabel);
+        final EditText creditNumberInput = (EditText) view.findViewById(R.id.creditNumberInput);
+        final EditText monthInput = (EditText) view.findViewById(R.id.monthInput);
+        final EditText cvvNumberInput = (EditText) view.findViewById(R.id.cvvNumberInput);
+        final Button submitPayment = (Button) view.findViewById(R.id.submitPayment);
+
+        //fetch custom app font
+        Typeface typeFace = Typeface.createFromAsset(getActivity().getAssets(),"fonts/FredokaOne-Regular.ttf");
+
+        //set logo font style
+        pageHeader.setTypeface(typeFace);
+        costLabel.setTypeface(typeFace);
+        cardNumberLabel.setTypeface(typeFace);
+        expDateLabel.setTypeface(typeFace);
+        cvvLabel.setTypeface(typeFace);
+
+        //set cost label
+        // TODO: 2017-03-17 Get tutoring session cost from bundle 
+        tutoringCost.setText("$99.99");
+
+        //submit payment
+        submitPayment.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isValid = true;
+                creditCarNumber = creditNumberInput.getText().toString();
+                expirationMonth = monthInput.getText().toString();
+                cvvNumbers = cvvNumberInput.getText().toString();
+
+                //validate inputs
+                if(creditCarNumber.length() != 12) {
+                    creditNumberInput.setError("Credit card number required!");
+                    isValid = false;
+                }
+
+                if(expirationMonth.length() != 4) {
+                    monthInput.setError("Expiration required!");
+                    isValid = false;
+                }
+
+                if(cvvNumbers.length() != 3) {
+                    cvvNumberInput.setError("CVV required!");
+                    isValid = false;
+                }
+
+            }
+        });
 
         return view;
     }
