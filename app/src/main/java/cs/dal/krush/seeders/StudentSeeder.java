bad773818afd0c85faa45d1ed8e196282ffe314c
Orjan Monsen <mons@dal.ca>
2017-03-26 16:54:47 -0300

    setup-tutor-list-item-views: fixed image display bug

--- app/src/main/java/cs/dal/krush/seeders/StudentSeeder.java
+++ app/src/main/java/cs/dal/krush/seeders/StudentSeeder.java
@@ -13,16 +13,16 @@ public class StudentSeeder {
      * @param db
      */
     public static void insert(DBHelper db){
-        db.student.insert(1,"egg1","Greg","Miller","gregpmillr@gmail.com","password");
-        db.student.insert(3,"egg1","John","Wick","jw@gmail.com","password");
-        db.student.insert(2,"egg1","John","Smith","js@gmail.com","password");
-        db.student.insert(4,"egg1","Samuel","Jackson","sj@dal.ca","password");
-        db.student.insert(5,"egg1","Jack","Daniels","jd@outlook.com","password");
-        db.student.insert(6,"egg1","Tim","Thompson","tt@hotmail.ca","password");
-        db.student.insert(7,"egg1","Mike","Jackson","mj@hotmail.ca","password");
-        db.student.insert(8,"egg1","Lucy","Patterson","lp@hotmail.ca","password");
-        db.student.insert(7,"egg1","Carline","Jenkins","mj@hotmail.ca","password");
-        db.student.insert(3,"egg1","McKenzie","Rekker","mk@dal.ca","password");
-        db.student.insert(4,"egg1","Darlene","Tims","dt@gmail.ca","password");
+        db.student.insert(1, null, "Greg", "Miller", "gregpmillr@gmail.com", "password");
+        db.student.insert(3, null, "John", "Wick", "jw@gmail.com", "password");
+        db.student.insert(2, null, "John", "Smith", "js@gmail.com", "password");
+        db.student.insert(4, null, "Samuel", "Jackson", "sj@dal.ca", "password");
+        db.student.insert(5, null, "Jack", "Daniels", "jd@outlook.com", "password");
+        db.student.insert(6, null, "Tim", "Thompson", "tt@hotmail.ca", "password");
+        db.student.insert(7, null, "Mike", "Jackson", "mj@hotmail.ca", "password");
+        db.student.insert(8, null, "Lucy", "Patterson", "lp@hotmail.ca", "password");
+        db.student.insert(7, null, "Carline", "Jenkins", "mj@hotmail.ca", "password");
+        db.student.insert(3, null, "McKenzie", "Rekker", "mk@dal.ca", "password");
+        db.student.insert(4, null, "Darlene", "Tims", "dt@gmail.ca", "password");
     }
 }
