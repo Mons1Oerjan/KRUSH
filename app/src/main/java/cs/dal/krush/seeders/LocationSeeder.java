18b013f1ac786b9b61587c5f1472cf015cc897fb
Eric Desjardins <eric.desjardins11@gmail.com>
2017-03-26 20:04:37 -0300

    Updated session seeder's location

--- app/src/main/java/cs/dal/krush/seeders/LocationSeeder.java
+++ app/src/main/java/cs/dal/krush/seeders/LocationSeeder.java
@@ -12,11 +12,11 @@ public class LocationSeeder {
      * @param db
      */
     public static void insert(DBHelper db){
-        db.location.insert("Halifax");
-        db.location.insert("Stewiacke");
-        db.location.insert("Truro");
-        db.location.insert("Bridgewater");
-        db.location.insert("Dartmouth");
-        db.location.insert("Wolfville");
+        db.location.insert("6050 University Avenue B3H");
+        db.location.insert("5991 Spring Garden Road, B3H 1Y6");
+        db.location.insert("5440 Spring Garden Road, B3J 1E9");
+        db.location.insert("6225 University Avenue, B3H 4H8");
+        db.location.insert("6136 University Avenue, B3H 4J2");
+        db.location.insert("1451 South Park Street, B3J OB6");
     }
 }
--- app/src/main/java/cs/dal/krush/seeders/TutoringSessionSeeder.java
+++ app/src/main/java/cs/dal/krush/seeders/TutoringSessionSeeder.java
@@ -114,13 +114,12 @@ public class TutoringSessionSeeder {
         calendarEndTime.set(Calendar.MINUTE, 25);
         startTime = timeFormatter.format(calendarStartTime.getTime());
         endTime = timeFormatter.format(calendarEndTime.getTime());
-        db.tutoringSession.insert(1,1,1,1,"Meeting at Coburg Coffee", startTime, endTime);
-        db.tutoringSession.insert(1,6,6,1,"Meeting at Acadia library", startTime, endTime);
-        db.tutoringSession.insert(1,3,3,1,"Meeting at Truro Big Stop", startTime, endTime);
-        db.tutoringSession.insert(1,2,1,1,"Meeting at Dal Math Learning Centre", startTime, endTime);
-        db.tutoringSession.insert(1,2,1,1,"Meeting at Dal Killam Library", startTime, endTime);
-        db.tutoringSession.insert(1,3,1,1,"Meeting at Public Library 4th floor", startTime, endTime);
-        db.tutoringSession.insert(1,5,1,1,"Meeting at Dal CS Learning Centre", startTime, endTime);
-        db.tutoringSession.insert(1,6,1,1,"Meeting at Dal Student Union Building", startTime, endTime);
+        db.tutoringSession.insert(1,1,2,1,"Meeting at Starbucks Coffee", startTime, endTime);
+        db.tutoringSession.insert(1,6,2,1,"Meeting at Starbucks Coffee", startTime, endTime);
+        db.tutoringSession.insert(1,3,6,1,"Meeting at Humani-T Cafe", startTime, endTime);
+        db.tutoringSession.insert(1,2,6,1,"Meeting at Humani-T Cafe", startTime, endTime);
+        db.tutoringSession.insert(1,3,4,1,"Meeting at Killam Library", startTime, endTime);
+        db.tutoringSession.insert(1,5,4,1,"Meeting at Killam Library", startTime, endTime);
+        db.tutoringSession.insert(1,6,3,1,"Meeting at Public Library", startTime, endTime);
     }
 }
